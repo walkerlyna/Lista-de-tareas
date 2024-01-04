0649db1bfb589e2ff590c3124ae6a53a13957d88
Walkerlyna <sotomanriquez36@gmail.com>
2024-01-03 22:38:44 -0700

    npm run build3

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles.css'
-import { Tasks } from './components/tasks'
+import { Tasks } from './tasks'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
